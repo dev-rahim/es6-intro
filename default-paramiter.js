c497afe48b4aad09d997ec4753fdbe71bde66241
dev-rahim <rahimuddin5915@gmail.com>
2022-09-08 00:16:19 +0600

    default-paramiter

---
+++ default-paramiter.js
@@ -0,0 +1,11 @@
+function add(num1 = 5, num2 = 0) {
+    // num2 = num2 || 0;
+    // return num1 + num2;
+    // if (num2 == undefined) {
+    //     num2 = 0;
+    // }
+    console.log(num1 + num2);
+
+}
+const result = add();
+// console.log(result);
