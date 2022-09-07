bd0444b865365ef067ea1356e4c8de6e7677214d
dev-rahim <rahimuddin5915@gmail.com>
2022-09-08 00:34:30 +0600

    tamplate string

---
+++ tamplate-string.js
@@ -0,0 +1,17 @@
+const Rahim = 'asif akber';
+const nafiza = "Maye tumi dhukkho chino";
+// const multilineText1 = "
+// ami
+// tmi &
+//     amra
+// ";
+const multilineText = `
+ami 
+tmi &
+amra
+`;
+console.log(multilineText);
+const RahimsRoll = 166;
+const RahimsAge = 16;
+console.log("My name is Rahim. My class roll is = ${RahimsRoll}");
+console.log(`My name is Rahim. My class roll is = ${RahimsRoll}. I am ${RahimsAge}years old.`);
